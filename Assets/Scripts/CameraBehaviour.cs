c7ecb5718126b083832eaf7b41cd0e69c6b1948d
Nadel1 <nataliapio@yahoo.de>
2020-08-26 19:41:09 +0200

    added camera behaviour

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Transform follow;
+    public float moveSpeed = 0.1f;
+    private Vector3 moveTo;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveTo = new Vector3(follow.position.x, follow.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, moveTo, moveSpeed*Time.fixedDeltaTime);
+    }
+}
