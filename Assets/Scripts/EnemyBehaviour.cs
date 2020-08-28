af8b9291d01f869eb20b6f248b4ec582e11df32a
Nadel1 <nataliapio@yahoo.de>
2020-08-28 16:38:31 +0200

    added shooting and moving to enemy

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
     public float health = 100;
+    public Vector2 speed;
+    private Rigidbody2D rb;
+    private Transform target;
+    private Vector2 targetVector;
+    private Vector2 forward;
     // Start is called before the first frame update
     void Start()
     {
+        rb = this.GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").gameObject.transform;
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+        if (Vector2.Distance(target.position, transform.position) > 3)
+        {
+            
+            targetVector = target.position;
+            forward = new Vector2(rb.transform.right.x, rb.transform.right.y);
+            rb.MovePosition(rb.position + forward * speed * Time.fixedDeltaTime);
+
+        }
+        //transform.LookAt(target.position)
+        //transform.right = (target.position - transform.position);
+        Quaternion rotation = Quaternion.LookRotation
+            (target.position - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform shootFrom;
+    public float waitBetweenShots = 0.5f;
+    private bool alreadyWaiting = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!alreadyWaiting)
+        {
+            Instantiate(projectile, shootFrom.position, this.gameObject.transform.rotation);
+            StartCoroutine(WaitBetweenShots());
+        }
+    }
+    IEnumerator WaitBetweenShots()
+    {
+        alreadyWaiting = true;
+        yield return new WaitForSeconds(waitBetweenShots);
+        alreadyWaiting = false;
+    }
+}
